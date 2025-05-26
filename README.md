69b11d7c393fc555069a45fec5168efc62bd472a
Yasser Momade <yassermomade11@gmail.com>
2025-05-26 14:05:41 +0200

    primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# marcacaovagas
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/marcacaovagas/marcacaovagas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.marcacaovagas.marcacaovagas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
